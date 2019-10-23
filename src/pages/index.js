f9505c23df16b33575c05dc18c854a0995415beb
colpaerte <eli.colpaert@delaware.pro>
2019-10-23 16:01:50 +0200

    Remove unneeded components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import ActionCard from "../components/actionCard"
-import PricingCard from "../components/pricingCard"
 import Hero from "../components/hero"
-import CallToAction from "../components/callToAction"
 import SEO from "../components/seo"
 import BlackShirt from "../components/images/black-shirt.js"
 import PinkShirt from "../components/images/pink-shirt.js"
@@ -11,27 +8,26 @@ import InstaFeed from "../components/instaFeed.js"
 import { Link } from "gatsby"
 
 class IndexPage extends React.Component {
-
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       result: {
         graphql: {
           user: {
             edge_owner_to_timeline_media: {
-              edges: []
-            }
-          }
-        }
-      }
+              edges: [],
+            },
+          },
+        },
+      },
     }
   }
   componentDidMount() {
-    fetch('https://www.instagram.com/kwaly_/?__a=1')
+    fetch("https://www.instagram.com/kwaly_/?__a=1")
       .then(res => res.json())
-      .then((data) => {
-        this.setState({ result: data });
-        console.log(this.state.result);
+      .then(data => {
+        this.setState({ result: data })
+        console.log(this.state.result)
       })
       .catch(console.log)
   }
@@ -42,13 +38,9 @@ class IndexPage extends React.Component {
         <SEO title="KWALY - ORGANIC RECYCLED CLOTHING" />
         <Hero></Hero>
         <section className="bg-white py-8 my-8">
-
           <div className="lg:w-5/12 mx-auto">
-
             <div className="mx-auto text-center text-karla-uppercase text-2xl">
-              <h2 className="mb-8">
-                EMBROIDERED SERIES
-              </h2>
+              <h2 className="mb-8">EMBROIDERED SERIES</h2>
               <div className="flex justify-center shadow-xl p-4">
                 <div className="flex-1 px-8">
                   <PinkShirt />
@@ -58,25 +50,24 @@ class IndexPage extends React.Component {
                 </div>
               </div>
               <div>
-
                 <button className="text-karla-uppercase bg-gray-300 p-4 text-lg shadow-lg -mt-4">
                   <Link to="./shop">Find out more!</Link>
                 </button>
               </div>
             </div>
           </div>
-
         </section>
         <section>
           <div className="lg:w-10/12 mx-auto">
-
-            <InstaFeed edges={this.state.result.graphql.user.edge_owner_to_timeline_media.edges} />
-
+            <InstaFeed
+              edges={
+                this.state.result.graphql.user.edge_owner_to_timeline_media
+                  .edges
+              }
+            />
           </div>
-
-
         </section>
-      </Layout >
+      </Layout>
     )
   }
 }
