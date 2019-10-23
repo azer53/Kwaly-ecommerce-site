9f1b38d2f26a3467ae05690eefa4b76bd853ef35
colpaerte <eli.colpaert@delaware.pro>
2019-10-23 17:35:55 +0200

    product service calling

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,5 @@
 const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const productService = require(`./src/utils/productService.js`)
 /**
  * Implement Gatsby's Node APIs in this file.
  *
@@ -9,40 +9,40 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 // Implement the Gatsby API “createPages”. This is called once the
 // data layer is bootstrapped to let plugins create pages from data.
 exports.createPages = async ({ graphql, actions, reporter }) => {
-    const { createPage } = actions
-    // Query for markdown nodes to use in creating pages.
-    const result = await graphql(
-      `
+  const { createPage } = actions
+  // Query for markdown nodes to use in creating pages.
+  const result = await graphql(
+    `
       query {
         allContentfulClothes {
-            edges {
+          edges {
             node {
-                slug
+              slug
             }
-            }
+          }
         }
-        }
-      `
-    )
-    // Handle errors
-    if (result.errors) {
-      reporter.panicOnBuild(`Error while running GraphQL query.`)
-      return
-    }
-
-    // Create pages for each product.
-    const productTemplate = path.resolve(`src/templates/productTemplate.js`)
-    console.log(result);
-    result.data.allContentfulClothes.edges.forEach(({ node }) => {
-      const productSlug = node.slug
-      createPage({
-        path: 'shop/'+productSlug,
-        component: productTemplate,
-        // In your blog post template's graphql query, you can use path
-        // as a GraphQL variable to query for data from the markdown file.
-        context: {
-          productSlug,
-        },
-      })
-    })
+      }
+    `
+  )
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
   }
+
+  // Create pages for each product.
+  const productTemplate = path.resolve(`src/templates/productTemplate.js`)
+  result.data.allContentfulClothes.edges.forEach(({ node }) => {
+    const productSlug = node.slug
+    productService.createProduct()
+    createPage({
+      path: "shop/" + productSlug,
+      component: productTemplate,
+      // In your blog post template's graphql query, you can use path
+      // as a GraphQL variable to query for data from the markdown file.
+      context: {
+        productSlug,
+      },
+    })
+  })
+}
--- src/utils/productService.js
+++ src/utils/productService.js
@@ -3,16 +3,20 @@ require("dotenv").config({
 })
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
-export const createProduct = () => {
-  stripe.products.create(
-    {
-      name: "T-shirt",
-      type: "good",
-      description: "Comfortable cotton t-shirt",
-      attributes: ["size", "gender"],
-    },
-    function(err, product) {
-      // asynchronously called
-    }
-  )
+module.exports = {
+  createProduct: function() {
+    console.log("entering create product")
+    stripe.products.create(
+      {
+        name: "T-shirt",
+        type: "good",
+        description: "Comfortable cotton t-shirt",
+        attributes: ["size", "gender"],
+      },
+      function(err, product) {
+        console.log(err)
+      }
+    )
+  },
+  otherMethod: function() {},
 }
