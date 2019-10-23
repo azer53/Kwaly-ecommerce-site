864c0d88bee9653ae2610912b9547450b24d75d7
colpaerte <eli.colpaert@delaware.pro>
2019-10-24 00:42:53 +0200

    create query working, update not yet

--- src/utils/productService.js
+++ src/utils/productService.js
@@ -5,41 +5,72 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 module.exports = {
   createProduct: function(product) {
-    createBaseProducts(product)
-  },
-  productExists: function() {
-    return true
+    if (productExists(product.slug)) {
+      updateBaseProduct(product)
+    } else {
+      createBaseProducts(product)
+    }
   },
 }
 
 function createSkus(variants, baseProductStripeId) {
-  //replace with foreach
-  variant = variants[0]
-  stripe.skus.create(
-    {
-      product: baseProductStripeId,
-      attributes: { size: variant.size },
-      price: variant.price,
-      currency: "eur",
-      inventory: { type: "finite", quantity: variant.initialStockLevel },
-    },
-    function(err, sku) {
-      console.log(err)
-    }
-  )
+  variants.forEach(function(variant) {
+    stripe.skus.create(
+      {
+        product: baseProductStripeId,
+        attributes: {
+          size: variant.size,
+          name: baseProductStripeId + "-" + variant.size,
+        },
+        price: variant.price * 100,
+        currency: "eur",
+        inventory: { type: "finite", quantity: variant.initialStockLevel },
+      },
+      function(err, sku) {
+        console.log(err)
+      }
+    )
+  })
 }
 
 function createBaseProducts(baseProduct) {
-  console.log(baseProduct)
   stripe.products.create(
     {
-      name: baseProduct.title,
+      id: baseProduct.slug,
+      name: baseProduct.slug,
       type: "good",
-      attributes: ["size"],
+      attributes: ["size", "name"],
     },
     function(err, product) {
       console.log(err)
       createSkus(baseProduct.variants, product.id)
     }
   )
+}
+
+function updateBaseProduct(baseProduct) {
+  //images and descriptions can be changed here
+  stripe.products.update(baseProduct.title, function(err, product) {
+    console.log(err)
+    baseProduct.variants.forEach(function(variant) {
+      if (!skuExists(product.id + "-" + variant.size)) {
+        var variants = [variant]
+        createSkus(variants, product.id)
+      }
+    })
+  })
+}
+
+function skuExists(skuId) {
+  stripe.skus.retrieve(skuId, function(err, sku) {
+    // asynchronously called
+    if (sku) return true
+  })
+}
+
+function productExists(productId) {
+  stripe.products.retrieve(productId, function(err, product) {
+    // asynchronously called
+    if (product) return true
+  })
 }
