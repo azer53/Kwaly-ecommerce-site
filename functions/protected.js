b18c29ad7cad1c74c5528bf827986dc7e5801ba8
colpaerte <eli.colpaert@delaware.pro>
2019-10-28 18:49:11 +0100

    first functions added

--- functions/protected.js
+++ functions/protected.js
@@ -1,9 +1,9 @@
 exports.handler = async (event, context) => {
   /* Reading the context.clientContext will give us the current user */
-  const claims = context.clientContext && context.clientContext.user
-  console.log("User claims", claims)
-  if (!claims) {
-    console.log("No claims! Begone!")
+  const authorized = context.clientContext && context.clientContext.user
+  console.log("User authorized", authorized)
+  if (!authorized) {
+    console.log("No valid authorization! Request denied!")
     return {
       statusCode: 401,
       body: JSON.stringify({
@@ -14,7 +14,7 @@ exports.handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Super secret`,
+      message: `Request granted`,
     }),
   }
 }
