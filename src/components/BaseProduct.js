a3b41f8e7031f81a43585c1a500d5bb37344fff4
colpaerte <eli.colpaert@delaware.pro>
2019-10-30 13:11:34 +0100

    admin page added

---
+++ src/components/BaseProduct.js
@@ -0,0 +1,36 @@
+import React from "react"
+
+const BaseProduct = props => {
+  console.log(props)
+  const baseProduct = props.product
+  const skus = props.skus
+  return (
+    <div>
+      <h3 className="text-3xl">{baseProduct.name}</h3>
+      <table class="table-fixed my-8">
+        <thead>
+          <tr>
+            <th class="w-1/2 px-4 py-2">Sku Id</th>
+            <th class="w-1/4 px-4 py-2">Initial Stock</th>
+            <th class="w-1/4 px-4 py-2">Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {skus.map((item, index) => {
+            const sku = item.node
+            return (
+              <tr>
+                {/* <tr class="bg-gray-100"> */}
+                <td class="border px-4 py-2"> {sku.attributes.size}</td>
+                <td class="border px-4 py-2">{sku.inventory.quantity}</td>
+                <td class="border px-4 py-2">{sku.price / 100}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default BaseProduct
--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,16 +1,29 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import BaseProduct from "../components/BaseProduct"
 import { useIdentityContext } from "react-netlify-identity-widget"
 import { Redirect } from "@reach/router" // highlight-line
 
-const AdminPage = () => {
+const AdminPage = ({ data }) => {
   const identity = useIdentityContext() // see https://github.com/sw-yx/react-netlify-identity for api of this identity object
   const isLoggedIn = identity && identity.isLoggedIn
-  if (isLoggedIn)
+  const baseProducts = data.allStripeProduct.edges
+  const allSkus = data.allStripeSku.edges
+
+  if (!isLoggedIn)
     return (
       <Layout>
-        <h1>Admin</h1>
+        <div className="container mx-auto pt-4 pb-12">
+          {baseProducts.map((product, index) => {
+            const skus = allSkus.filter(
+              variant => variant.node.product.id === product.node.id
+            )
+            return (
+              <BaseProduct key={index} product={product.node} skus={skus} />
+            )
+          })}
+        </div>
       </Layout>
     )
   else
@@ -19,5 +32,35 @@ const AdminPage = () => {
       <Redirect to={`/404`} noThrow />
     )
 }
+
+export const query = graphql`
+  {
+    allStripeProduct {
+      edges {
+        node {
+          name
+          id
+        }
+      }
+    }
+    allStripeSku {
+      edges {
+        node {
+          id
+          product {
+            id
+          }
+          inventory {
+            quantity
+          }
+          price
+          attributes {
+            size
+          }
+        }
+      }
+    }
+  }
+`
 
 export default AdminPage
