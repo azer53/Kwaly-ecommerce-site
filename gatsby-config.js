26c469c34c0701422bd407e7d33cfab4577776bf
Raphael <turminetor@gmail.com>
2019-11-13 16:11:26 +0100

    kwaly icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,13 +49,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Commerce 2020`,
-        short_name: `starter`,
+        name: `Kwaly Webshop`,
+        short_name: `Kwaly Webshop`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/kwaly-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
