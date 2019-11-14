b42b7ec34b0ada79ec6216ab775deda97532d5ea
Raphael <turminetor@gmail.com>
2019-11-14 17:04:25 +0100

    extra padding

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ class Footer extends React.Component {
 
 <footer className="bg-white py-10 text-center text-sm">
 	<span >©KWALY - {new Date().getYear() + 1900}</span>
-    <div className="text-center">
+    <div className="my-4 text-center">
       <Link to="/termsandconditions">Terms & Conditions</Link>
     </div>
 </footer>
