e7984591e3881f991d69b531b08124b06d5fb2f2
colpaerte <eli.colpaert@delaware.pro>
2019-11-20 13:14:37 +0100

    accessibility fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,44 +5,40 @@ import MasterCardLogo from "../images/mastercardLogo.svg"
 import BancontactLogo from "../images/bancontact-logo.svg"
 import EnvIcons from "../components/images/environment-icons"
 
-
-
 export default function Footer() {
-
-
   return (
     <div>
-        <div className="border-t mx-auto">
-          <span className="text-lg block text-center mx-auto p-8">
-            Standards we support with our clothing:
-          </span>
-          <EnvIcons></EnvIcons>
+      <div className="border-t mx-auto">
+        <span className="text-lg block text-center mx-auto p-8">
+          Standards we support with our clothing:
+        </span>
+        <EnvIcons></EnvIcons>
+      </div>
+      <footer className="bg-gray-200 border-t-2 mt-6 py-10 text-center text-sm">
+        <span>©KWALY - {new Date().getYear() + 1900}</span>
+        <div className="my-4 text-center">
+          <Link to="/termsandconditions" className="hover:underline mx-4">
+            Terms & Conditions
+          </Link>
+          <Link to="/gdpr" className="hover:underline mx-4">
+            Privacy Policy
+          </Link>
         </div>
-        <footer className="bg-gray-200 border-t-2 mt-6 py-10 text-center text-sm">
-          <span>©KWALY - {new Date().getYear() + 1900}</span>
-          <div className="my-4 text-center">
-            <Link to="/termsandconditions" className="hover:underline mx-4">
-              Terms & Conditions
-            </Link>
-            <Link to="/gdpr" className="hover:underline mx-4">
-              Privacy Policy
-            </Link>
-          </div>
-          <div className="p-8">
-            <span className="text-gray-600 italic">We Accept:</span>
-            <div className="flex justify-center">
-              <div className="w-24">
-                <VisaLogo></VisaLogo>
-              </div>
-              <div className="w-24">
-                <MasterCardLogo></MasterCardLogo>
-              </div>
-              <div className="w-24">
-                <BancontactLogo></BancontactLogo>
-              </div>
+        <div className="p-8">
+          <span className="text-gray-800 italic">We Accept:</span>
+          <div className="flex justify-center">
+            <div className="w-24">
+              <VisaLogo></VisaLogo>
+            </div>
+            <div className="w-24">
+              <MasterCardLogo></MasterCardLogo>
+            </div>
+            <div className="w-24">
+              <BancontactLogo></BancontactLogo>
             </div>
           </div>
-        </footer>
-      </div>
+        </div>
+      </footer>
+    </div>
   )
 }
